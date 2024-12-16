93a807c2eecf3e05b4549e6823933f94daddd48f
aviel <aviel469@gmail.com>
2024-12-16 11:33:49 +0200

    router done

---
+++ server/src/index.ts
@@ -0,0 +1,16 @@
+import express from 'express'
+import cors from 'cors'
+import expenseRouter  from './routes/expense.route'
+import settingRouter  from './routes/settings.route'
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/expense',expenseRouter)
+app.use('/api/settings',settingRouter)
+
+
+app.listen(3000,()=>{
+    console.log(`[server] I'm up on port 3000`)
+})
