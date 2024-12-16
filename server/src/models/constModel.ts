d32695c418ea6e3b0b07b821ea76d0f0d8b737bd
aviel <aviel469@gmail.com>
2024-12-16 11:12:17 +0200

    move types to the right unlazt place

--- server/src/models/constModel.ts
+++ server/src/models/constModel.ts
@@ -1,10 +1,9 @@
 import { model, Schema } from "mongoose";
 
-const constSchema = new Schema ({
+export const constSchema = new Schema ({
     description:String,
     amount:Number,
 
 },{timestamps:true}
 )
-export type Const = ReturnType<() => typeof constSchema>
 const ConstModel = model('Const' , constSchema)
