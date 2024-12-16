56e0b8b1d55471bb4b4d6610d69d5777084fc25b
aviel <aviel469@gmail.com>
2024-12-16 17:10:24 +0200

    connect wiirtes settings

--- server/src/controllers/settings.controller.ts
+++ server/src/controllers/settings.controller.ts
@@ -1,8 +1,9 @@
 import { NextFunction, Request, Response } from "express";
+import { getSettings as get ,patchSettings  } from "../services/settings.service";
 
 export const updateSetting = async (req:Request , res : Response , next:NextFunction) =>{
     try {
-        
+        res.json(await patchSettings(req.body))
     } catch (err) {
         console.error("Can't update setting" ,err)
         next(err)
@@ -10,7 +11,7 @@ export const updateSetting = async (req:Request , res : Response , next:NextFunc
 }
 export const getSetting = async (req:Request , res : Response , next:NextFunction) =>{
     try {
-        
+       res.json( await get())
     } catch (err) {
         console.error("Can't get setting" ,err)
         next(err)
